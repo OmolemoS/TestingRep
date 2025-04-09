69b39af4f9728f946b2f09c61d40b1e4f69c5cf3
OmolemoS <OmolemoS@Lab-17.kegrp.co.za>
2025-04-09 09:52:19 +0200

    First commit

---
+++ src/main/java/com/mycompany/gittesting/Gittesting.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gittesting;
+
+/**
+ *
+ * @author OmolemoS
+ */
+public class Gittesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.print("Hello Boy");
+    }
+}
